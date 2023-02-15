e8b679af67ce0068044f5a1fd8892b7619a22133
yuuAn <nora@yuuan.net>
2023-02-15 21:09:07 +0900

    Session の設定

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: process.env.APP_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7days
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();

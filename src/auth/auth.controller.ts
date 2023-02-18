4f6216744a52b3a6f60ec534c5ccbd87a20b230a
yuuAn <nora@yuuan.net>
2023-02-18 16:19:41 +0900

    ログイン中のユーザーの情報を返すルートを追加

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import {
   Controller,
   UseGuards,
+  Get,
   Post,
   HttpCode,
   Request,
 } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
+import { AuthenticatedGuard } from './authenticated.guard';
 
 @Controller()
 export class AuthController {
@@ -13,6 +15,12 @@ export class AuthController {
   @Post('auth/login')
   @HttpCode(200)
   async login(@Request() request: any) {
+    return request.user;
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('auth/me')
+  async me(@Request() request: any) {
     return request.user;
   }
 

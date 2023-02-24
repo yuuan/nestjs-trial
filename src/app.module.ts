f650e3c1d4949ad877e369b2f1be9a287f971be5
yuuAn <nora@yuuan.net>
2023-02-25 00:34:48 +0900

    Mailer の設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { RegistrationModule } from './registration/registration.module';
 import { BooksModule } from './books/books.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [AuthModule, RegistrationModule, BooksModule],
+  imports: [AuthModule, RegistrationModule, BooksModule, MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/mail/mail.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: parseInt(process.env.MAIL_PORT, 10),
+        auth: {
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      },
+      defaults: {
+        from: process.env.MAIL_FROM,
+      },
+      template: {
+        dir: join(__dirname, '/templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+})
+export class MailModule {}

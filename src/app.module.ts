1a70d35279776c89a61dae70d284d5d9f9edc0fe
yuuAn <nora@yuuan.net>
2023-02-24 22:46:10 +0900

    ユーザー登録をする API を作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { RegistrationModule } from './registration/registration.module';
 import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [AuthModule, BooksModule],
+  imports: [AuthModule, RegistrationModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/registration/register-user.dto.ts
@@ -0,0 +1,24 @@
+import { IsString, IsEmail, MaxLength, IsNotEmpty, IsStrongPassword } from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  readonly name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MaxLength(200)
+  readonly email: string;
+
+  @IsString()
+  @MaxLength(50)
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+  readonly password: string;
+}

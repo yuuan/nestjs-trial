653ff74437e77cc0cf3d7afd653411b6e5302a75
yuuAn <nora@yuuan.net>
2023-03-01 23:40:28 +0900

    Server の設定で ConfigService を使う

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  await app.listen(3000);
+  await app.listen(configService.get<number>('app.port'));
 }
 bootstrap();

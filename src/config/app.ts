1f98d2b592b262986c1140a6af3e9aa2bb7a4efc
yuuAn <nora@yuuan.net>
2023-03-01 23:15:55 +0900

    設定ファイルを作成

---
+++ src/config/app.ts
@@ -0,0 +1,6 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs('app', () => ({
+  secret: process.env.APP_SECRET,
+  port: parseInt(process.env.APP_SERVER_PORT, 10) || 3000,
+}));

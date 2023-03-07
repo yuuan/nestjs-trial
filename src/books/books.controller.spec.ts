6275a832ee548a04b467ebe437413767528cc8b4
yuuAn <nora@yuuan.net>
2023-03-07 23:38:22 +0900

    BooksController のテストを作成

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,18 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { BooksController } from './books.controller';
+import { BooksModule } from './books.module';
+import { BooksService } from './books.service';
 
 describe('BooksController', () => {
-  let controller: BooksController;
+  const isbn = '9784774196053';
+  const responseBody = ['success'];
+  const mockedBooksService = { find: () => responseBody };
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [BooksController],
-    }).compile();
+  let booksController: BooksController;
 
-    controller = module.get<BooksController>(BooksController);
-  });
+  describe('show', () => {
+    beforeEach(async () => {
+      const moduleRef = await Test.createTestingModule({
+        imports: [BooksModule],
+      })
+        .overrideProvider(BooksService)
+        .useValue(mockedBooksService)
+        .compile();
+      booksController = moduleRef.get<BooksController>(BooksController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+    it('should return the bibliographic infomation for the specified book', async () => {
+      const response = await booksController.show(isbn);
+
+      expect(response).toEqual(responseBody);
+    });
   });
 });

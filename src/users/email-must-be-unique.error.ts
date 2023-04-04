cfb91a75d37d7e55550b4a23c9def6a8f30f0400
yuuAn <nora@yuuan.net>
2023-04-04 14:29:49 +0900

    Lint を適用

--- src/users/email-must-be-unique.error.ts
+++ src/users/email-must-be-unique.error.ts
@@ -1,7 +1,7 @@
 export class EmailMustBeUniqueError extends Error {
   constructor(
     public value: string,
-    message: string = `email '${value}' is already registered.`,
+    message = `email '${value}' is already registered.`,
   ) {
     super(message);
     this.name = 'EmailMustBeUniqueError';

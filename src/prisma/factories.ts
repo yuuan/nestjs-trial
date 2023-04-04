dc27a9cfb98b21cef617cac6aba3bf5d623c848a
yuuAn <nora@yuuan.net>
2023-04-04 09:11:44 +0900

    FileFactory を作成

--- src/prisma/factories.ts
+++ src/prisma/factories.ts
@@ -1,4 +1,11 @@
-import { defineUserFactory } from './fabbrica';
+import { defineUserFactory, defineFileFactory } from './fabbrica';
 export { initialize } from './fabbrica';
 
 export const UserFactory = defineUserFactory();
+
+export const FileFactory = defineFileFactory({
+  defaultData: {
+    author: UserFactory,
+    content: Buffer.from('ABC'),
+  },
+});
